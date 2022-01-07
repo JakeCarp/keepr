a0af7fd3e3f8a9e9a5954692e15f1ec2affbd67f
JakeCarp <jakecarpenter407@gmail.com>
2022-01-07 13:50:23 -0700

    vaults repo

--- keepr/Repositories/VaultsRepository.cs
+++ keepr/Repositories/VaultsRepository.cs
@@ -1,7 +1,71 @@
+using System;
+using System.Data;
+using System.Linq;
+using Dapper;
+using keepr.Models;
+
 namespace keepr.Repositories
 {
     public class VaultsRepository
     {
-        
+        private readonly IDbConnection _db;
+        internal Vault Create(Vault newVault)
+        {
+            var sql = @"
+            INSERT INTO vaults(name, description, isPrivate)
+            VALUES(@Name, @Description, @IsPrivate)
+            SELECT LAST_INSERT_ID();";
+            var id = _db.ExecuteScalar<int>(sql);
+            return newVault;
+        }
+
+        internal Vault GetById(int id)
+        {
+            var sql = @"
+            SELECT 
+                v.*,
+                a.*
+            FROM vaults v
+            JOIN accounts a ON v.creatorId = a.id
+            WHERE v.id = @id
+            ;";
+            return _db.Query<Vault, Account, Vault>(sql, (v, a) =>
+            {
+                v.Creator = a;
+                return v;
+            }).FirstOrDefault();
+        }
+
+        internal object Update(Vault vault)
+        {
+
+            var sql = @"
+            UPDATE vaults
+                SET
+                    name = @Name,
+                    description = @Description,
+                    isPrivate = @IsPrivate,
+            WHERE id = @id LIMIT 1;";
+            var rowsAffected = _db.Execute(sql, vault);
+            if (rowsAffected > 1)
+            {
+                //alert sysadmin
+                throw new Exception("THE BIG BAD, ALTER SYSADMIN");
+            }
+            if (rowsAffected < 1)
+            {
+                throw new Exception("Update Failed, Likely Bad Id");
+            }
+            return vault;
+        }
+
+        internal void Delete(int vaultId)
+        {
+            var sql = @"
+            DELETE FROM vaults WHERE id = @vaultId
+            ;";
+            _db.Execute(sql, new { vaultId });
+
+        }
     }
 }
--- keepr/Services/VaultsService.cs
+++ keepr/Services/VaultsService.cs
@@ -1,28 +1,59 @@
 using System;
 using keepr.Models;
+using keepr.Repositories;
 
 namespace keepr.Services
 {
     public class VaultsService
     {
+
+        private readonly VaultsRepository _repo;
+
+        public VaultsService(VaultsRepository repo)
+        {
+            _repo = repo;
+        }
+
         internal Vault Create(Vault newVault)
         {
-            throw new NotImplementedException();
+            return _repo.Create(newVault);
         }
 
         internal Vault GetById(int id)
         {
-            throw new NotImplementedException();
+            Vault vault = _repo.GetById(id);
+            if (vault == null)
+            {
+                throw new Exception("Bad Vault Id");
+            }
+            return vault;
         }
 
         internal object Update(Vault update, string id)
         {
-            throw new NotImplementedException();
+            var vault = isVaultOwner(id, update.Id);
+            vault.Name = update.Name ?? vault.Name;
+            vault.Description = update.Description ?? vault.Description;
+            vault.IsPrivate = update.IsPrivate;
+            return _repo.Update(vault);
+
         }
 
-        internal object Delete(int id1, string id2)
+        internal object Delete(int vaultId, string userId)
         {
-            throw new NotImplementedException();
+            Vault vault = isVaultOwner(userId, vaultId);
+            _repo.Delete(vaultId);
+            return "Delete Successful";
+        }
+
+        internal Vault isVaultOwner(string userId, int vaultId)
+        {
+            Vault vault = GetById(vaultId);
+            if (vault.CreatorId != userId)
+            {
+                throw new Exception("This is not Your Vault");
+            }
+            return vault;
         }
     }
 }
