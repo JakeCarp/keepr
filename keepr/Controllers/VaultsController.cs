b24c18acd8b07852058a2048ac81094e7aa0d3ff
JakeCarp <jakecarpenter407@gmail.com>
2022-01-07 12:34:53 -0700

    Vault Controller

--- keepr/Controllers/VaultsController.cs
+++ keepr/Controllers/VaultsController.cs
@@ -1,7 +1,92 @@
+using System;
+using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
+using keepr.Models;
+using keepr.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace keepr.Controllers
 {
-    public class VaultsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VaultsController : ControllerBase
     {
-        
+        private readonly VaultsService _vs;
+
+        public VaultsController(VaultsService vs)
+        {
+            _vs = vs;
+        }
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<Vault>> CreateVault([FromBody] Vault newVault)
+        {
+            try
+            {
+                var userInfo = await HttpContext.GetUserInfoAsync<Account>();
+                newVault.CreatorId = userInfo.Id;
+                Vault vault = _vs.Create(newVault);
+                vault.Creator = userInfo;
+                return Ok(vault);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Vault> GetVaultById(int id)
+        {
+            try
+            {
+                var vault = _vs.GetById(id);
+                return Ok(vault);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+
+            }
+        }
+
+        [HttpPut("{id}")]
+        [Authorize]
+
+        public async Task<ActionResult<Vault>> UpdateVault([FromBody] Vault update, int id)
+        {
+            try
+            {
+                var userInfo = await HttpContext.GetUserInfoAsync<Account>();
+                var vault = _vs.Update(update, userInfo.Id);
+                return Ok(vault);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+
+        public async Task<ActionResult<String>> DeleteVault(int id)
+        {
+            try
+            {
+                var userInfo = await HttpContext.GetUserInfoAsync<Account>();
+                var res = _vs.Delete(id, userInfo.Id);
+                return Ok(res);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
     }
 }
--- keepr/Services/VaultsService.cs
+++ keepr/Services/VaultsService.cs
@@ -1,7 +1,28 @@
+using System;
+using keepr.Models;
+
 namespace keepr.Services
 {
     public class VaultsService
     {
-        
+        internal Vault Create(Vault newVault)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Vault GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Update(Vault update, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Delete(int id1, string id2)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
