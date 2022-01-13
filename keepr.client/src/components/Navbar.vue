2376e6af10332e951136d0c8e3ca65e95ddfcae4
JakeCarp <jakecarpenter407@gmail.com>
2022-01-13 11:36:58 -0700

    done?

--- keepr.client/src/components/Navbar.vue
+++ keepr.client/src/components/Navbar.vue
@@ -11,7 +11,7 @@
   >
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+        <img alt="logo" src="../assets/img/heroku-logo.p" height="45" />
       </div>
     </router-link>
     <form>
